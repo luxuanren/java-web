ba6d4b35add30472cef7a453c2ecd069e49a1dfd
luxuanren <867773467@qq.com>
2016-09-29 00:12:34 +0800

    add a class
    add a class

---
+++ src/com/luxuanren/study/servlet/BookItem.java
@@ -0,0 +1,32 @@
+package com.luxuanren.study.servlet;
+
+public class BookItem {
+	private String title;
+	private String author;
+	private String price;
+	private String pulishDate;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getPulishDate() {
+		return pulishDate;
+	}
+	public void setPulishDate(String pulishDate) {
+		this.pulishDate = pulishDate;
+	}
+}
